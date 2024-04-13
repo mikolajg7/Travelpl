648f3313b2e307eb48bf6cbc8c8699250b16a806
Kusieq <166519581+Kusieq@users.noreply.github.com>
2024-04-13 15:21:33 +0200

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def login_view(request):
+    return render(request, 'login.html')
+def registration_view(request):
+    return render(request, 'registration.html')
