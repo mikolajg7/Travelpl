c02e1666cbf819fa6e2e6e776628f72d0f0fc621
Mikolaj Golanski <mikolajgolanski16@gmail.com>
2024-04-14 10:23:03 +0200

    adding posts added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('registration/', views.registration_view, name='registration'),
+    path('add_post/', views.add_post, name='add_post'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms.models import model_to_dict
+from .utils import get_random_posts
+from .forms import PostForm
+from .models import Post
+import requests
 
+posts = get_random_posts()[:5]
 def home(request):
-    return render(request, 'home.html')
+    if 'new_post' in request.session:
+        new_post_id = request.session['new_post']['id']
+        new_post = Post.objects.get(id=new_post_id)
+        posts.insert(0, model_to_dict(new_post))
+        del request.session['new_post']
+    return render(request, 'home.html', {'posts': posts})
+
+def add_post(request):
+    if request.method == 'POST':
+        content = request.POST.get('body')
+        new_post = Post.objects.create(body=content)
+        request.session['new_post'] = model_to_dict(new_post)
+        return redirect('home')
+
 def login_view(request):
     return render(request, 'login.html')
 def registration_view(request):
