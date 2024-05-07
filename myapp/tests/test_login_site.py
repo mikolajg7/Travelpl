7a08752a718a27ad80799091eb7f8bc79ad3900e
Mikołaj Golański <148275198+mikolajg7@users.noreply.github.com>
2024-05-07 12:19:06 +0200

    Delete myapp/tests/test_login_site.py

--- myapp/tests/test_login_site.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-
-class LoginPageTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_login_page_opens_correctly(self):
-        response = self.client.get('/login/') 
-        self.assertEqual(response.status_code, 200)
